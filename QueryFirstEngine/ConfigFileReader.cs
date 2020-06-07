09e776398aa622f1e7430fece4dc799fe41eecd2
bbsimonbb <bbsimonbb@gmail.com>
2020-06-07 19:23:40 +1000

    New .net standard proj compiles

--- QueryFirstEngine/ConfigFileReader.cs
+++ QueryFirstEngine/ConfigFileReader.cs
@@ -42,14 +42,4 @@ namespace QueryFirst
             else return null;
         }
     }
-    
-
-    public class QFConfigModel
-    {
-        public string defaultConnection { get; set; }
-        public string provider { get; set; }
-        public string helperAssembly { get; set; }
-        public bool makeSelfTest { get; set; }
-        public bool connectEditor2DB { get; set; }
-    }
-}
+ }
---
+++ QueryFirstEngine/QfConfigModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryFirst
+{
+    public class QFConfigModel
+    {
+        public string defaultConnection { get; set; }
+        public string provider { get; set; }
+        public string helperAssembly { get; set; }
+        public bool makeSelfTest { get; set; }
+        public bool connectEditor2DB { get; set; }
+    }
+}
